b00d164c3eba1acb354ff9b1fb01c1a88ea564cc
matthewsullivan1 <143414874+matthewsullivan1@users.noreply.github.com>
2023-11-01 16:43:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,48 @@
-import binascii
 from Block import Block
 from Aes import Aes
-from utils import Sbox, InvSbox, Rcon
-
-def KeyExpansion(key, Nk=4, Nb=4, Nr=10) -> list:
-        #Generates 4*(Nr + 1) words
-        #Linear array of words, denoted w[i], 0 <= i <= 4*(Nr + 1)
-
-        # 44 words for 128 bit key
-    w = [0] * (4 * (Nr + 1))
-
-        # Copying initial key to first four words of expanded key
-    w[0] = key[0:4]
-    w[1] = key[4:8]
-    w[2] = key[8:12]
-    w[3] = key[12:16]
-
-    i = Nk
-    while(i < Nb*(Nr+1)):
-        temp = w[i-1]     
-        if i % Nk == 0:
-            temp = SubWord(RotWord(temp))
-            temp[0] ^= Rcon[(i//Nk)]
-
-        #for larger keys
-        #elif(Nk > 6 and i % Nk == 4):
-            #temp = self.SubWord(temp)
-
-        w[i] = [w[i-Nk][j] ^ temp[j] for j in range(4)]
-        i += 1
-    return w
-
-def RotWord(word):
-    #[a0,a1,a2,a3] -> [a1,a2,a3,a0]
-    return word[1:] + word[:1]
-
-def SubWord(word):
-    return [Sbox[word[0]], Sbox[word[1]], Sbox[word[2]], Sbox[word[3]]]
-
-
-
-
+from utils import Sbox, InvSbox, Rcon, KeyExpansion, display
 
 if __name__ == '__main__':
-    key = [0x2b,0x7e,0x15,0x16,
-           0x28,0xae,0xd2,0xa6,
-           0xab,0xf7,0x15,0x88,
-           0x09,0xcf,0x4f,0x3c]
+    key = [0x2B,0x7E,0x15,0x16,
+           0x28,0xAE,0xD2,0xA6,
+           0xAB,0xF7,0x15,0x88,
+           0x09,0xCF,0x4F,0x3C]
     
     
-    state = [[0x32, 0x88, 0x31, 0xe0],
-             [0x43, 0x5a, 0x31, 0x37],
-             [0xf6, 0x30, 0x98, 0x07],
-             [0xa8, 0x8d, 0xa2, 0x34]]
+    state = [[0x32, 0x88, 0x31, 0xE0],
+             [0x43, 0x5A, 0x31, 0x37],
+             [0xF6, 0x30, 0x98, 0x07],
+             [0xA8, 0x8D, 0xA2, 0x34]]
 
-    expanded_key = KeyExpansion(key)
+    #expanded_key = KeyExpansion(key)
 
-    test = Aes(state, expanded_key)
+    
+    
+    #test = Aes("C:/Users/18163/Desktop/Github AES/AES/data.txt", "C:/Users/18163/Desktop/Github AES/AES/output.txt", key)
+    
+
+    '''
+    with open("C:/Users/18163/Desktop/Github AES/AES/data.txt", "rb") as file:
+        byte = file.read(1)
+        print(byte)
+    
     test.Encrypt()
-    #print(test.state)
+    for word in test.state:
+        for byte in word:
+            print(hex(byte))
+        print("\n")
 
-    ss = [[0]*4]*4
-    print(ss)
+    for i in range(1,10):
+        print(i)
+    '''
+
+    # Initialize an empty 4x4 state matrix filled with zeros
+    state = [[0x00, 0x00, 0x00, 0x00],
+            [0x00, 0x00, 0x00, 0x00],
+            [0x00, 0x00, 0x00, 0x00],
+            [0x00, 0x00, 0x00, 0x00]]
+
+    with open('C:/Users/18163/Desktop/Github AES/AES/data.txt', 'r') as fp:
+        hex_list = ["{:02x}".format(ord(c)) for c in fp.read()]
+
+    print(hex_list)
